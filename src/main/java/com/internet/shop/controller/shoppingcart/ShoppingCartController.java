f0c21609ca4b5e52584f0fcff6fef0cc74a8c176
AlexeyBavika <bavikaalexey@gmail.com>
2020-04-28 17:10:08 +0300

    added registration, adding products and adding products to shopping cart

---
+++ src/main/java/com/internet/shop/controller/shoppingcart/ShoppingCartController.java
@@ -0,0 +1,26 @@
+package com.internet.shop.controller.shoppingcart;
+
+import com.internet.shop.lib.Injector;
+import com.internet.shop.service.ShoppingCartService;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/shoppingCart")
+public class ShoppingCartController extends HttpServlet {
+    private static final Long USER_ID = 1L;
+    private static final Injector injector = Injector.getInstance("com.internet.shop");
+    private final ShoppingCartService shoppingCartService =
+            (ShoppingCartService) injector.getInstance(ShoppingCartService.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+            IOException {
+        req.setAttribute("products", shoppingCartService.getAllProducts(shoppingCartService
+                .getByUserId(USER_ID)));
+        req.getRequestDispatcher("/WEB-INF/view/shoppingcart/shoppingcart.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/shoppingcart/shoppingcart.jsp
@@ -0,0 +1,30 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>Shopping cart : </h1>
+<table border="1">
+    <tr>
+        <th>Id</th>
+        <th>Name</th>
+        <th>Price</th>
+    </tr>
+    <c:forEach var="product" items="${products}">
+    <tr>
+        <td>
+            <c:out value="${product.id}"/>
+        </td>
+        <td>
+            <c:out value="${product.name}"/>
+        </td>
+        <td>
+            <c:out value="${product.price}"/>
+        </td>
+    </tr>
+    </c:forEach>
+</body>
+</html>
